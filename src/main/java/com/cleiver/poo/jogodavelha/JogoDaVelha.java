02a62940339e613fb4e0b2a0dfcfb7e53bce8a53
Cleiver <creibis10@gmail.com>
2023-11-02 13:08:54 -0300

    metodos de classes com parametros no classico jogo da velha

---
+++ src/main/java/com/cleiver/poo/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,51 @@
+package com.cleiver.poo.jogodavelha;
+public class JogoDaVelha {
+
+    char[][] jogoVelha = new char[3][3];
+    int jogada = 1;
+
+    boolean validarJogada(int linha, int coluna, char sinal) {
+
+        if (!(jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O')) {
+            jogoVelha[linha][coluna] = sinal;
+            jogada++;
+            return true;
+        } else { //jogada inválida
+            return false;
+        }
+    }
+
+    void imprimirTabuleiro() {
+        for (int i = 0; i < jogoVelha.length; i++) {
+            for (int j = 0; j < jogoVelha[i].length; j++) {
+                System.out.print(jogoVelha[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    boolean verificarGanhador(char sinal) {
+        if ((jogoVelha[0][0] == sinal && jogoVelha[0][1] == sinal && jogoVelha[0][2] == sinal) || //linha 1
+                (jogoVelha[1][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[1][2] == sinal) || //linha 2
+                (jogoVelha[2][0] == sinal && jogoVelha[2][1] == sinal && jogoVelha[2][2] == sinal) || //linha 3
+                (jogoVelha[0][0] == sinal && jogoVelha[1][0] == sinal && jogoVelha[2][0] == sinal) || //coluna 1
+                (jogoVelha[0][1] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][1] == sinal) || //coluna 2
+                (jogoVelha[0][2] == sinal && jogoVelha[1][2] == sinal && jogoVelha[2][2] == sinal) || //coluna 3
+                (jogoVelha[0][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][2] == sinal) || //diagonal
+                (jogoVelha[0][2] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][0] == sinal)) {  //diagonal inversa
+            return true;
+        }
+        return false;
+    }
+
+    char vezDoJogador(){
+        if (jogada % 2 == 1){
+            System.out.println("Vez do jogador 1. Escolha linha e coluna (1-3).");
+            return 'X';
+        }else {
+            System.out.println("Vez do jogador 2. Escolha linha e coluna (1-3).");
+            return 'O';
+        }
+
+    }
+}
---
+++ src/main/java/com/cleiver/poo/jogodavelha/Main.java
@@ -0,0 +1,61 @@
+package com.cleiver.poo.jogodavelha;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        JogoDaVelha jogoDaVelha = new JogoDaVelha();
+
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
+
+        boolean ganhou = false;
+        char sinalJogada;
+        int linha = 0;
+        int coluna = 0;
+
+        while (!ganhou) {
+
+            sinalJogada = jogoDaVelha.vezDoJogador();
+
+            linha = valor("Linha", scan);
+            coluna = valor("Coluna", scan);
+
+            if (!jogoDaVelha.validarJogada(linha, coluna, sinalJogada)){
+                System.out.println("Jogada inválida, tente novamente");
+            }
+
+            jogoDaVelha.imprimirTabuleiro();
+
+            if (jogoDaVelha.verificarGanhador('X')){
+                ganhou = true;
+                System.out.println("Parabéns, jogador 1 ganhou!");
+            } else if (jogoDaVelha.verificarGanhador('O')){
+                ganhou = true;
+                System.out.println("Parabéns, jogador 2 ganhou!");
+            } else if (jogoDaVelha.jogada > 9){
+                ganhou = true;
+                System.out.println("Ninguém ganhou essa partida.");
+            }
+        }
+    }
+
+    static int valor(String tipoValor, Scanner scan) {
+        int valor = 0;
+        boolean valorValida = false;
+        while (!valorValida) {
+            System.out.println("Entre com a " + tipoValor + " (1, 2 ou 3)");
+            valor = scan.nextInt();
+            if (valor >= 1 && valor <= 3) {
+                valorValida = true;
+            } else {
+                System.out.println("Entrada inválida, tente novamente");
+            }
+        }
+        valor--;
+        return valor;
+    }
+}
